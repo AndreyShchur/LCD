0e0687dc03cf600b0bf84cbaec42a4a5f5766bf7
AndreyShchur <77545634+AndreyShchur@users.noreply.github.com>
2021-09-19 19:30:57 +0300

    First commit

---
+++ lcd_lib.c
@@ -0,0 +1,16 @@
+// LCD
+// ver 0.0.1
+
+#include "lcd_lib.h"
+
+void LCDLIB_Init(void)
+{
+  
+}
+
+unsigned char LCDLIB_WriteStr(unsigned char lcdID,
+                              unsigned char *str, 
+                              unsigned int len)
+{
+  
+}
